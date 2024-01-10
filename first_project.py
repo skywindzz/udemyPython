5d11f157ced3a9137217f072340457b9c249cfdd
skywindzz <skywindzz@yahoo.com>
2024-01-09 16:03:57 -0800

    python day 1 excercise

--- first_project.py
+++ first_project.py
@@ -1,3 +1,26 @@
 print("hello world")
 print(100 + 50)
 print('hello world "hello"')
+print("hello" + " world", "how are" + " " + "you")
+# this is how you comment
+
+'''
+multiline
+comments
+'''
+
+# how to create special charater it's telling python to take the next charater as is
+print("my name is \"Anthony\"")
+print("this is\'t sparta")
+# new line inside print
+print("this a line\nand this is another line")
+
+# to display a backslash
+print("this is \\ backslash")
+#add a tab in print
+print("\tthis is the third line")
+
+# asking for user's input and then print it
+print('your name is ' + input('input your first name:') + ' '  + input('input your last name:'))
+
+print(input('what are you learning today?'))
